bcad4fc418910b5c7da5ce17aac2f5bcc335cce6
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-03-21 16:20:31 +0700

    koneksi ke database sql server

--- CRUDonline/Form1.cs
+++ CRUDonline/Form1.cs
@@ -13,5 +13,8 @@ namespace CRUD04on
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=YUDHO\\YUDHO;Initial Catalog=CRUDSEDERHANA;Integrated Security=True";
+
+       
 
 }
