375af3b7e4947431bb5a448a055100d66dcb89a1
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-03-21 16:33:15 +0700

    Membuat tabel kosong dan di isi dengan data dari query

--- CRUDonline/Form1.cs
+++ CRUDonline/Form1.cs
@@ -48,4 +48,8 @@ namespace CRUD04on
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+
 }
